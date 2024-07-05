7fbfbf92b0ef99e0e649b4283c76e97b54efc001
hrithik <hrithikraj0001@gmail.com>
2024-07-05 18:25:13 +0530

    docker added

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { User, _ } = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 var validator = require("validator");
